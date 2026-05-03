c63714a3247b9bbec8a15f5b888afaa573375846
Jesutol <jesusenavaf@gmail.com>
2026-05-02 23:29:56 -0300

    assa

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import os
 import discord
 from discord.ext import commands
-from datetime import date
+from datetime import datetime, date
+import zoneinfo
 
-
+# Fecha de salida GTA 6
 GTA6_RELEASE_DATE = date(2026, 11, 19)
 
+# Zona horaria Argentina
+ZONA = zoneinfo.ZoneInfo("America/Argentina/Buenos_Aires")
+
+# Intents necesarios
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='$', intents=intents)
 
+# Comando de prueba
 @bot.command()
 async def test(ctx, arg):
     await ctx.send(arg)
 
+# Comando GTA 6
 @bot.command()
 async def gta6(ctx):
-    today = date.today()
+    today = datetime.now(ZONA).date()
     days_left = (GTA6_RELEASE_DATE - today).days
 
     if days_left > 0:
@@ -27,9 +34,16 @@ async def gta6(ctx):
     else:
         await ctx.send(f"GTA 6 salio hace {-days_left} dias.")
 
+# Evento cuando el bot está listo
 @bot.event
 async def on_ready():
-    print( "Bot is ready!" )
-    print('------')
+    print("Bot is ready!")
+    print("------")
 
-bot.run(os.getenv("TOKEN"))   
+# Obtener token desde Railway
+TOKEN = os.getenv("TOKEN")
+
+if TOKEN is None:
+    print("ERROR: No se encontró el TOKEN")
+else:
+    bot.run(TOKEN)
